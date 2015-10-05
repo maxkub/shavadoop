a3e6edabc0cde9e728d00bba1e997fed127ef260
mkubryk <maxime.kubryk@gmail.com>
2015-10-05 14:55:51 +0200

    Master : testWorker is add but remains to be improved

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.io.IOException;
+
 
 public class Main {
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException, InterruptedException
 	{
 		System.out.println("Hello");
+		
+		Master master = new Master();
+		
+		master.importWorkerIds(args[0]);
+		
+		master.testWorkers();
+		
 	}
 
 }
--- src/Master.java
+++ src/Master.java
@@ -1,4 +1,89 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Master {
+	
+	private ArrayList<String> workerIds = new ArrayList<String>();
+	
+	
+	public Master()
+	{
+	}
+	
+	public void importWorkerIds(String fileName)
+	{
+		/*
+		 * read file containing the name of workers
+		 */
+				
+        File file = new File(fileName);
+	    
+	    try 
+	    {
+	        Scanner sc = new Scanner(file);   
+
+	        while (sc.hasNextLine()) 
+	        {
+	            String i = sc.nextLine();
+	            workerIds.add(i);
+	        }
+	        sc.close();
+	    } 
+	    catch (FileNotFoundException e) 
+	    {
+	        e.printStackTrace();
+	    }
+	}
+	
+	
+	public void testWorkers() throws IOException, InterruptedException
+	{
+		/*
+		 * Check each worker's disponibility  
+		 */
+		
+		Process p;
+		String s;
+		String status = "-1";
+		
+		for(String worker : workerIds)
+		{
+			String[] command = {"/bin/sh", "-c", "nc -z "+worker+" 22 > /dev/null | echo $?"};
+			p = Runtime.getRuntime().exec(command);
+			
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			
+	        while ((s = br.readLine()) != null)
+	        	
+	        	if(s.equals("0"))
+	        	{
+	        		status = "available";
+	        	}
+	        	else
+	        	{
+	        		status = "out";
+	        		workerIds.remove(worker);
+	        	}
+	        
+	        System.out.println(worker+" status: " + status);
+	        
+	        p.waitFor();
+	        System.out.println ("exit: " + p.exitValue());
+	        p.destroy();
+	        
+		}
+		
+		
+		
+		
+		
+	}
 
 }
